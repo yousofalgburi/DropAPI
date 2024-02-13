d26a3e118801aa4727779b1d299ad536a72b4a96
Yousof Algburi <yousofalgburi@gmail.com>
2024-02-13 16:54:53 -0500

    mysql setup

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,8 @@
+using MySql.Data.MySqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(builder.Configuration["ConnectionStrings:Default"]));
 builder.Services.AddControllers();
 
 var app = builder.Build();
