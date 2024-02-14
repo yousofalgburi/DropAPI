77e6b403d9baa0784191ff2b135e9dfd328ccdeb
Yousof Algburi <yousofalgburi@gmail.com>
2024-02-14 18:56:11 -0500

    init frontend auth0 setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
-import { Button } from './components/ui/button'
-import { ThemeProvider } from './components/theme-provider'
+import { useAuth0 } from '@auth0/auth0-react'
 import { ModeToggle } from './components/mode-toggle'
+import { ThemeProvider } from './components/theme-provider'
 
 function App() {
+	const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0()
+
+	if (isLoading) {
+		return <div>Loading...</div>
+	}
+	if (error) {
+		return <div>Oops... {error.message}</div>
+	}
+
 	return (
 		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-			<Button>Test</Button>
+			{isAuthenticated && user ? (
+				<div>
+					Hello {user.name}{' '}
+					<button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+						Log out
+					</button>
+				</div>
+			) : (
+				<button onClick={() => loginWithRedirect()}>Log in</button>
+			)}
+
 			<ModeToggle />
 		</ThemeProvider>
 	)
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -5,6 +6,14 @@ import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<Auth0Provider
+			domain={`${import.meta.env.VITE_AUTH0_DOMAIN}`}
+			clientId={`${import.meta.env.VITE_AUTH0_CLIENT_ID}`}
+			authorizationParams={{
+				redirect_uri: window.location.origin,
+			}}
+		>
+			<App />
+		</Auth0Provider>
 	</React.StrictMode>
 )
