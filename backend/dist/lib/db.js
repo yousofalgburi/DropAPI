eaece67dd34bdf89f3807c526d2de401a2add66c
Yousof Algburi <yousofalgburi@gmail.com>
2024-03-28 22:52:14 -0400

    delete bad dist

--- backend/dist/lib/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_postgres_1 = require("drizzle-orm/node-postgres");
-const xata_1 = require("./xata");
-const xata = (0, xata_1.getXataClient)();
-const db = (0, node_postgres_1.drizzle)(xata);
-exports.default = db;
--- backend/dist/routes/apps.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appsRoutes = void 0;
-const apps_controller_1 = require("../controllers/apps.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-async function appsRoutes(server) {
-    server.post('/apps', { preHandler: [auth_middleware_1.authMiddleware] }, apps_controller_1.createApp);
-}
-exports.appsRoutes = appsRoutes;
