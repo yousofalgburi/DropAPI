982cd261aad08bb944a2df8346814993658167b1
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-12 19:04:31 -0400

    db setup

---
+++ api/db/index.ts
@@ -0,0 +1,23 @@
+import postgres from 'postgres'
+
+const sql = postgres(process.env.DATABASE_URL, {
+	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+	max: 10,
+	idle_timeout: 20,
+	connect_timeout: 30,
+	prepare: true,
+	onnotice: console.log,
+	debug: process.env.NODE_ENV !== 'production' ? console.log : false,
+	connection: {
+		application_name: 'dropapi',
+	},
+	fetch_types: true,
+})
+
+process.on('SIGINT', async () => {
+	console.log('Closing database connections...')
+	await sql.end({ timeout: 5 })
+	process.exit(0)
+})
+
+export default sql
